2d72da5d825da31075655c4431bd8dbf5e1ff075
kamrul <kamrul4h3190@gmail.com>
2025-10-29 21:41:41 +0600

    leetcode 121

---
+++ Dynamic_Programming/src/Leetcode_121.java
@@ -0,0 +1,29 @@
+public class Leetcode_121 {
+    public static void main(String[] args) {
+        Leetcode_121 app = new Leetcode_121();
+        int[] prices = {7,1,5,3,6,4}; int k = 1;
+        System.out.println("maximum stock profit : "+app.maxProfit(prices));
+    }
+
+    public int maxProfit(int[] prices) {
+        n=prices.length;    memProfit = new Integer[2][n]; //2 row,n col
+        return dynamicTrade(0,1,0,prices); //at day 0, only allowed to buy
+    }
+    Integer[][] memProfit;  int n;
+    private int dynamicTrade(int day,int allowed,int trxNo,int[] prices){ // , allowed to sell:0
+        if (day>=n || trxNo >= 1) return 0; // only 1 transaction allowed
+        if (memProfit[allowed][day] != null ) return memProfit[allowed][day];
+
+        //state machine dp, Calls will be done based on states
+        if (allowed==1){ //allowed to buy:1
+            int buy = -prices[day] + dynamicTrade(day+1, 0, trxNo,prices); //need to invest ,profit will reduce.after buy allowed to sell(0)
+            int skip = dynamicTrade(day+1, allowed,trxNo, prices); //do not buy today
+            return memProfit[allowed][day] = Math.max(buy,skip);
+        }
+        else { //allowed to sell:0
+            int sell = prices[day] + dynamicTrade(day+1, 1, trxNo+1,prices) ; //sells ,profit will increase.after sell allowed to buy(1)
+            int skip = dynamicTrade(day+1, allowed, trxNo,prices); //do not buy today
+            return memProfit[allowed][day] = Math.max(sell,skip);
+        }
+    }
+}
